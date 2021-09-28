c9110482f000f9f07db9c438f9a0afc8c4f707cb
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2021-09-28 17:30:09 +0100

    Alias feature replaces shared feature
    Aliases allow for a list of people to be replace by writing the alias instead

--- main.py
+++ main.py
@@ -6,43 +6,39 @@
 #
 # Created:     20/09/2021
 # Copyright:   (c) AOs Productions 2021 - 2021
-# Version:	   0.1.1
+# Version:	   0.2
 #-------------------------------------------------------------------------------
+import re
 with open("bill.txt") as file:
     rawBill = file.readlines()
 
 billList = []
 people = {}
-shared = {}
+aliases = {}
 totalCost = 0
 
-shared = {"shared":{"total": 0,"items": []}} # Sets the default indicator for shared items between everyone mentioned
 for line in rawBill:
     if line[-1:] == "\n": # Removes new line characters
         line = line[:-1]
-    item = line.split(",")
-    if len(item) == 1: # Item of length one is read as a shared indicator to be used in future
-        shared = {item[0]: shared[list(shared.keys())[0]]} # Renames shared user indicator
-    else:
+    item = re.split(",| = ",line)
+    try:
         totalCost += float(item[0])
         billList.append(item) # Processed bill (unused so far)
+        if item[1] in aliases: # If alias found, it must be split into seperate people
+            rawItem = [item[0]] # Makes new item with all people individually noted
+            for person in aliases[item[1]]:
+                rawItem.append(person)
+            item = rawItem
         for x in range(1,len(item)):
-            #print(list(shared.keys())[0])
-            if item[x] == list(shared.keys())[0]:
-                shared[list(shared.keys())[0]]["total"] += float(item[0]) / (len(item) - 1)  # Adds money
-                shared[list(shared.keys())[0]]["items"].append(item)  # Adds item for reference
-            else:
-                if item[x] not in people: # If person has been noted down before
-                    people[item[x]] = {"total": 0,"items": []}
-                people[item[x]]["total"] += float(item[0])/(len(item)-1) # Adds money
-                people[item[x]]["items"].append(item) # Adds item for reference
+            if item[x] not in people: # If person has been noted down before
+                people[item[x]] = {"total": 0,"items": []}
+            people[item[x]]["total"] += float(item[0])/(len(item)-1) # Adds money
+            people[item[x]]["items"].append(item) # Adds item for reference
+    except ValueError: # Value assumed to be an alias indicator
+        aliases[item[0]] = item[1:] # Adds dictionary about the alias to the list of aliases
 
 for person in people:
-    print(float(shared[list(shared.keys())[0]]["total"]) / (len(people)))
-    people[item[x]]["total"] += float(shared[list(shared.keys())[0]]["total"]) / (len(people))  # Adds money
-    people[item[x]]["items"].append(shared[list(shared.keys())[0]]["items"])  # Adds item for reference
     print(person+"'s Total:"+"£"+str(round(people[person]["total"],2)))
-print(shared)
 
 print("--------------------")
 print("Grand Total:" + str(totalCost))
