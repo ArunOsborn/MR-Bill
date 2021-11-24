4845150144adf342a748cd5ef9637ccfcd149dad
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-11-24 23:26:34 +0000

    Equations can now be inputted as the price and some slight efficiency improvements although the program may run slightly slower overall now

--- main.py
+++ main.py
@@ -16,26 +16,28 @@ billList = []
 people = {}
 aliases = {}
 totalCost = 0
-
 for line in rawBill:
     if line[-1:] == "\n": # Removes new line characters
         line = line[:-1]
     item = re.split(" *, *| *= *",line)
     try:
-        totalCost += float(item[0])
-        billList.append(item) # Processed bill (unused so far)
-        if item[1] in aliases: # If alias found, it must be split into seperate people
-            rawItem = [item[0]] # Makes new item with all people individually noted
+        itemCost = eval(item[0])
+        totalCost += itemCost  # Eval used so calculations can be done directly on the input
+        billList.append(item)  # Processed bill (unused so far)
+        if item[1] in aliases:  # If alias found, it must be split into seperate people
+            rawItem = [item[0]]  # Makes new item with all people individually noted
             for person in aliases[item[1]]:
                 rawItem.append(person)
             item = rawItem
-        for x in range(1,len(item)):
-            if item[x] not in people: # If person has been noted down before
-                people[item[x]] = {"total": 0,"items": []}
-            people[item[x]]["total"] += float(item[0])/(len(item)-1) # Adds money
-            people[item[x]]["items"].append(item) # Adds item for reference
-    except ValueError: # Value assumed to be an alias indicator
-        aliases[item[0]] = item[1:] # Adds dictionary about the alias to the list of aliases
+        for x in range(1, len(item)):
+            if item[x] not in people:  # If person has been noted down before
+                people[item[x]] = {"total": 0, "items": []}
+            people[item[x]]["total"] += itemCost / (len(item) - 1)  # Adds money
+            people[item[x]]["items"].append(item)  # Adds item for reference
+    except NameError:  # Value assumed to be an alias indicator
+        aliases[item[0]] = item[1:]  # Adds dictionary about the alias to the list of aliases
+
+
 
 for person in people:
     print(person+"'s Total:"+"£"+str(round(people[person]["total"],2)))
