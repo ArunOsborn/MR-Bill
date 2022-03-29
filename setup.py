4537b296a6280da027bc2d8c4fee4f08f7f382c4
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2022-03-29 14:16:40 +0100

    Made setup.py file and moved main.py into mrbill folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='MR-Bill',
+   version='1.0',
+   description='A simple program for splitting bills from a receipt',
+   author='Arun Osborn',
+   author_email='arunosborn@gmail.com',
+   packages=['mrbill'],
+   install_requires=['re', 'json'], #External dependencies
+)
